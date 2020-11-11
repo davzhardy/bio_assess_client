9207d5358e7ac416012d9ccbf95e02a95ab84e69
David Hardy <david.hardy50@gmail.com>
2020-11-11 12:39:01 +0100

    feat: client connection to server

---
+++ src/ApiService.js
@@ -0,0 +1,21 @@
+const BASE_URL = 'http://localhost:3001';
+
+function getMazeOutput (instructions) {
+  return fetchRequest('/instructions', {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify(instructions)
+  })
+}
+
+function fetchRequest (path, options) {
+  return fetch(BASE_URL + path, options)
+    .then(res => res.status < 400 ? res.text() : Promise.reject(res))
+    .catch((err) => {
+      console.log('Error:', err) //eslint-disable-line no-console
+    })
+};
+
+export default {
+  getMazeOutput,
+};
--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import ApiService from './ApiService'
 
 function App() {
+
+  // const [output, setOutput] = useState([]);
+
+  // function createInstructions (string) {
+  //   ApiService.getMazeOutput({
+  //     instructions: string
+  //   })
+  //   .then(result => setOutput(result))
+  // }
+
+  // useEffect(() => {
+  //   createInstructions('n,w')
+  // },[]);
+
   return (
     <div>
       Test
+      {/* {output} */}
     </div>
   );
 }
